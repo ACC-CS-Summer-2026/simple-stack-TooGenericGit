323f0287ddcba6896e18faae6f0e233ab7934189
TooGenericGit <jinxtubeout@gmail.com>
2026-07-07 18:26:06 +0000

    Added required files and set up stack.h

---
+++ main.h
@@ -0,0 +1,11 @@
+/* *************************************************
+*  Name: Nikhil Prabhu
+*  Assignment: Simple Stack
+*  Purpose:
+*
+************************************************* */
+
+#ifndef ASSIGNMENT4_MAIN_H
+#define ASSIGNMENT4_MAIN_H
+
+#endif //ASSIGNMENT4_MAIN_H
---
+++ stack.h
@@ -0,0 +1,44 @@
+/* *************************************************
+*  Name: Nikhil Prabhu
+*  Assignment: Simple Stack
+*  Purpose: Establish headers for the stack
+*
+************************************************* */
+
+#ifndef ASSIGNMENT4_STACK_H
+#define ASSIGNMENT4_STACK_H
+#define STACKSIZE 10
+
+class Stack {
+
+public:
+
+    /**********************
+    Constructors/Destructor
+    ***********************/
+    Stack();
+    ~Stack();
+
+    /**********************
+    Methods
+    ***********************/
+    int pop();
+    int peek();
+    bool push(int);
+    bool isEmpty();
+
+private:
+
+    /**********************
+    Attributes
+    ***********************/
+    int top;
+    int stack[STACKSIZE];
+
+};
+
+
+
+
+
+#endif //ASSIGNMENT4_STACK_H
